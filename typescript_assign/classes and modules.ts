e052b4575bd479973250d00bb67c20f27c96156f
durgesh19952517 <65106278+durgesh19952517@users.noreply.github.com>
2022-05-23 07:27:59 +0000

    Add files via upload

---
+++ typescript_assign/classes and modules.ts
@@ -0,0 +1,36 @@
+class Account{
+    constructor(id,name,balance){
+        this.id=id;
+        this.name= name;
+        this.balance = balance;
+    }
+}
+class SavingAccount extends Account{
+   constructor(id,name,balance,interest){
+          super(id,name,balance);
+          this.interest  = interest;
+   }
+   getBalance(){
+       this.totalBalance=0;
+       this.totalBalance = this.balance + (this.balance*this.interest);
+       return this.totalBalance;
+   }
+}
+
+class CurrentAccount extends Account{
+   constructor(id,name,balance,cash_credit){
+          super(id,name,balance);
+          this.cash_credit  = cash_credit;
+   }
+   getBalance(){
+       this.totalBalance=0;
+       this.totalBalance = this.balance -this.cash_credit;
+       return this.totalBalance;
+   }
+}
+
+SavingAccountObj = new SavingAccount(11,"john",1500,10);
+console.log(SavingAccountObj.getBalance())
+
+CurrentAccountObj = new CurrentAccount(11,"jack",15000,500);
+console.log(CurrentAccountObj.getBalance())
---
+++ typescript_assign/destructuring.ts
@@ -0,0 +1,22 @@
+var names = ["alpha", "beta", "gamma", "delta"];
+var [firstelement,secondelement,thirdelement,fourthelement]= names;
+console.log(thirdelement);
+
+
+//Ans b.
+
+var organization ={
+    name:"Tom",
+    address:{
+        city:"Bangalore",
+        state:"Karnataka",
+        pincode:563221 
+    }
+}
+
+const {address:{city:city1,state:state1,pincode:pincode1}}=organization;
+
+console.log(pincode1);
+© 2022 GitHub, Inc.
+Terms
+Privac
